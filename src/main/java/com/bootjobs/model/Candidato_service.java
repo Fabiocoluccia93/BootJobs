e892f15aabd4d948f01759108e63e9d0477a9485
ISC_025 <ISC_025@ISC_025.homenet.telecomitalia.it>
2020-11-30 11:54:48 +0100

    Creazione Canidato e candidatura

--- src/main/java/com/bootjobs/model/Candidato_service.java
+++ src/main/java/com/bootjobs/model/Candidato_service.java
@@ -1,5 +1,33 @@
 package com.bootjobs.model;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
-public class Candidato_service {
+import model.Candidato;
 
+
+public class Candidato_service 
+{
+	  public boolean iscrizione(Candidato c_isc)
+	  {
+		 
+		  SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		  Session session = sessionFactory.openSession();
+		  session.beginTransaction();
+		  session.save(c_isc);
+		  System.out.println("Inserted Successfully");
+		  session.getTransaction().commit();
+		  session.close();
+		  sessionFactory.close();
+		  boolean check = false;
+		  return check;
+	  }
+	  public Candidato modificaProfilo(Candidato c_mod)
+	  {
+		  return c_mod;
+	  }
+	  public boolean eliminaProfilo(int id)
+	  {
+		  boolean check = false;
+		  return check;
+	  }
 }
