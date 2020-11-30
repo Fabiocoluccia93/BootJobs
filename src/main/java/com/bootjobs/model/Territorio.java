be8f70069ed5aa6dbe6bd67dddaee38174520e28
ISC_011 <ISC_011@LAPTOP-98FM46E8.home-life.hub>
2020-11-30 11:55:23 +0100

    prova 1

---
+++ src/main/java/com/bootjobs/model/Territorio.java
@@ -0,0 +1,32 @@
+package com.bootjobs.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class Territorio {
+
+	private String nome;
+
+	public Territorio() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Territorio(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	@Column(name = "")
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
