0c20b600664ad9d9ab6972a928958d11f6c07d28
ISC_011 <ISC_011@LAPTOP-98FM46E8.home-life.hub>
2020-12-03 22:55:40 +0100

    modifica

--- src/main/java/com/bootjobs/controller/Annuncio_controller.java
+++ src/main/java/com/bootjobs/controller/Annuncio_controller.java
@@ -6,9 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bootjobs.model.Annuncio;
+import com.bootjobs.model.Annuncio_Service;
+
 /**
  * Servlet implementation class Annuncio_controller
  */
+//@WebServlet("/Annuncio_controller")
 public class Annuncio_controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,15 +29,26 @@ public class Annuncio_controller extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		Annuncio a = new Annuncio();
+		Annuncio_Service as = new Annuncio_Service();
+		double x = Double.parseDouble(request.getParameter("stipendio"));
+		
+		a= as.findById(1l);
+		Annuncio b = new Annuncio();
+		b.setNome_annuncio(request.getParameter("nAnnuncio"));
+		b.setDescrizione(request.getParameter("descrizione"));
+		b.setStipendio(x);
+		b.setTipo_contratto(request.getParameter("tContratto"));
+		b.setTitolo_di_studio(request.getParameter("tStudio"));
+		as.inserisciAnnuncio(b);	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
+		
 	}
 
 }
