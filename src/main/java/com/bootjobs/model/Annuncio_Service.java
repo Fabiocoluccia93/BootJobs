cbf4025db5a6da8140c899e7570ae58f879b9972
ISC_011 <ISC_011@LAPTOP-98FM46E8.home-life.hub>
2020-12-03 22:49:13 +0100

    modifica

--- src/main/java/com/bootjobs/model/Annuncio_Service.java
+++ src/main/java/com/bootjobs/model/Annuncio_Service.java
@@ -1,22 +1,34 @@
 package com.bootjobs.model;
 
 import java.util.ArrayList;
-
 import javax.persistence.EntityManager;
-
-
+import javax.persistence.EntityTransaction;
 
 public class Annuncio_Service implements Annuncio_utility {
-	
+
+	public Annuncio findById(Long id) {
+		EntityManager em = Dao.newInstance().createEntityManager();
+		Annuncio a = new Annuncio();
+		em.getTransaction().begin();
+		a = em.find(Annuncio.class, id);
+		return a;
+	}
 	public Annuncio_Service() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public boolean inserisciAnnuncio(Annuncio a_ins) {
-		EntityManager em = Dao.newInstance().createEntityManager();
-		em.getTransaction().begin();
 		
-		return false;
+		EntityManager em = Dao.newInstance().createEntityManager();
+
+		EntityTransaction entr = em.getTransaction();
+		entr.begin();
+		em.persist(a_ins);
+		entr.commit();
+		em.close();
+		System.out.println(a_ins.getNome_annuncio());
+
+		return true;
 	}
 
 	public Annuncio modificaAnnuncio(Annuncio a_mod) {
